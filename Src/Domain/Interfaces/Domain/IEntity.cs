bdbd1ebb928cc1b8a67fcb4bdf6248499daebd8b
Alger <AlgerDu@outlook.com>
2021-06-13 19:24:57 +0800

    添加两个接口

---
+++ Src/Domain/Interfaces/Domain/IEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.ArtifactReposiotry
+{
+    /// <summary>
+    /// 领域模型 Entity，此接口只是用来某些情况下做标识使用
+    /// </summary>
+    public interface IEntity
+    {
+
+    }
+
+    /// <summary>
+    /// 领域模型 Entity，必须有唯一标识
+    /// </summary>
+    /// <typeparam name="TPrimaryKey">当使用多主键的时候，这里的模型可以是自身</typeparam>
+    public interface IEntity<TPrimaryKey> : IEntity
+        where TPrimaryKey : IEquatable<TPrimaryKey>
+    {
+        /// <summary>
+        /// 唯一标识
+        /// </summary>
+        TPrimaryKey PK { get; }
+    }
+}
---
+++ Src/Domain/Interfaces/Domain/IRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace D.ArtifactReposiotry
+{
+    /// <summary>
+    /// 仓储，只做标识用
+    /// </summary>
+    public interface IRepository
+    {
+    }
+
+    /// <summary>
+    /// 仓储
+    /// </summary>
+    /// <typeparam name="TEntity">实体类型</typeparam>
+    /// <typeparam name="TPrimaryKey">实体主键类型</typeparam>
+    public interface IRepository<TEntity, TPrimaryKey> : IRepository
+        where TEntity : class, IEntity<TPrimaryKey>
+        where TPrimaryKey : IEquatable<TPrimaryKey>
+    {
+        /// <summary>
+        /// 获取实体，进行操作；
+        /// 会独占实体，其它地方不能再继续跟新
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        TEntity Get(TPrimaryKey key);
+
+        /// <summary>
+        /// 将使用完成的 entity 放回 repository
+        /// </summary>
+        /// <param name="entity"></param>
+        void Put(TEntity entity);
+
+        /// <summary>
+        /// 插入
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        bool Insert(TEntity entity);
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        bool Delete(TPrimaryKey key);
+    }
+}
