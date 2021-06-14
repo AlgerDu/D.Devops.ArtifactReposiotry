82086314acf797e4fcf75d66019832e0c74c7816
Alger <AlgerDu@outlook.com>
2021-06-14 22:07:38 +0800

    创建仓储

--- Src/Domain/Domains/ArtifactRepo/ArtifactRepo.cs
+++ Src/Domain/Domains/ArtifactRepo/ArtifactRepo.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace D.ArtifactReposiotry
 {
-    public class ArtifactRepo
+    public class ArtifactRepo : IEntity<string>
     {
+        /// <summary>
+        /// Name
+        /// </summary>
+        [BsonId]
+        public string PK { get => Name; set { } }
 
+        public string Name { get; set; }
     }
 }
---
+++ Src/Domain/Domains/ArtifactRepo/IArtifactRepoRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.ArtifactReposiotry
+{
+    public interface IArtifactRepoRepository : IRepository<ArtifactRepo, string>
+    {
+    }
+}
