3360c2b420cd80082741c7bbe36810a23ac9d33a
Alger <AlgerDu@outlook.com>
2021-06-16 22:46:05 +0800

    Create RepositoryController.cs

---
+++ Src/Server/Controllers/RepositoryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D.ArtifactReposiotry.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]s")]
+    public class RepositoryController : ControllerBase
+    {
+        readonly ILogger _logger;
+        readonly IArtifactRepoRepository _artifactRepoRepository;
+
+        public RepositoryController(
+            ILogger<RepositoryController> logger
+            , IArtifactRepoRepository artifactRepoRepository
+            )
+        {
+            _logger = logger;
+            _artifactRepoRepository = artifactRepoRepository;
+        }
+
+        [HttpPost]
+        public ArtifactRepo PostTodoItem(ArtifactRepo artifactRepo)
+        {
+            var ok = _artifactRepoRepository.Insert(artifactRepo);
+
+            return artifactRepo;
+        }
+    }
+}
