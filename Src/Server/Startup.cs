4a0b16654a6e8f62b2c32debe85bd1e8eec06495
Alger <AlgerDu@outlook.com>
2021-06-16 22:46:02 +0800

    ioc

--- Src/Server/Startup.cs
+++ Src/Server/Startup.cs
@@ -25,6 +25,14 @@ namespace D.ArtifactReposiotry
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ILiteDB, DLiteDB>();
+
+            services.Configure<LiteDBOptions>((options) =>
+            {
+                options.Path = "./data/localdata.db";
+            });
+
+            services.AddSingleton<IArtifactRepoRepository, ArtifactRepoRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
