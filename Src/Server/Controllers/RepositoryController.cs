256589ccfb69515e908f708f6d00469e90c6c45d
Alger <AlgerDu@outlook.com>
2021-07-01 22:45:16 +0800

    Update RepositoryController.cs

--- Src/Server/Controllers/RepositoryController.cs
+++ Src/Server/Controllers/RepositoryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using D.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -13,22 +15,46 @@ namespace D.ArtifactReposiotry.Controllers
     {
         readonly ILogger _logger;
         readonly IArtifactRepoRepository _artifactRepoRepository;
+        readonly IMemoryCache _cache;
+        readonly IEntityAtomicProvider _entityAtomic;
 
         public RepositoryController(
             ILogger<RepositoryController> logger
             , IArtifactRepoRepository artifactRepoRepository
+            , ICacheProvider cacheProvider
+            , IEntityAtomicProvider entityAtomic
             )
         {
             _logger = logger;
+            _entityAtomic = entityAtomic;
+            _cache = cacheProvider.Get();
+
             _artifactRepoRepository = artifactRepoRepository;
         }
 
         [HttpPost]
-        public ArtifactRepo PostTodoItem(ArtifactRepo artifactRepo)
+        public IResult PostItem(ArtifactRepo artifactRepo)
         {
-            var ok = _artifactRepoRepository.Insert(artifactRepo);
+            var pk = artifactRepo.PK;
 
-            return artifactRepo;
+            using (var a = _entityAtomic.Get(pk))
+            {
+                if (a.IsOprting)
+                {
+                    return Result.CreateSuccess($"[{pk}] it is tring to add by others.");
+                }
+
+                var exist = _artifactRepoRepository.Get(pk);
+
+                if (exist != null)
+                {
+                    return Result.CreateError($"[{pk}] repo is aleardy exist.");
+                }
+
+                var ok = _artifactRepoRepository.Insert(artifactRepo);
+
+                return ok ? Result.CreateSuccess() : Result.CreateError("insert faild");
+            }
         }
     }
 }
