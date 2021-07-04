b07bad9fad02be17709a4e09b9e28f34da605c4f
Alger <AlgerDu@outlook.com>
2021-07-04 22:17:47 +0800

    mune

--- Src/H5Client/src/app/app.component.ts
+++ Src/H5Client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArtifactRepo, ArtifactRepoService } from './services/artifact-repo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isCollapsed = false;
+
+  artifactRepos: ArtifactRepo[] = [];
+
+  constructor(
+    private artifactRepoService: ArtifactRepoService
+  ){}
+
+  ngOnInit() {
+    this.artifactRepoService.get().subscribe(repos=>{
+      this.artifactRepos = repos;
+    });
+  }
 }
