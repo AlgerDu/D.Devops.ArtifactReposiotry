7edfe49f3dcd39bdacc9d0119babb708727f6bcf
Alger <AlgerDu@outlook.com>
2021-07-06 19:06:50 +0800

    Update app.module.ts

--- Src/H5Client/src/app/app.module.ts
+++ Src/H5Client/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconsProviderModule } from './icons-provider.module';
@@ -30,7 +30,7 @@ registerLocaleData(zh);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    FormsModule,ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     IconsProviderModule,
