2cf39cbd98fe3c5dfbdd8abf76a69bde189b2323
Alger <AlgerDu@outlook.com>
2021-07-07 22:57:47 +0800

    Update event.service.ts
    待测试

--- Src/H5Client/src/app/services/event.service.ts
+++ Src/H5Client/src/app/services/event.service.ts
@@ -1,28 +1,68 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-export interface Event {
+export interface DEvent {
   code: string;
   data: any;
+}
+
+export interface DEventHandleFunction {
+  (devent: DEvent): void;
+}
+export interface DEventHandler {
+  key: string;
+  code: string;
+  handle: DEventHandleFunction;
 }
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventService {
+export class DEventService {
 
-  constructor() { }
+  key: number;
+  handlers: { [key: string]: DEventHandler[]; };
 
-  subscribe(codes: string | string[]): string {
-    return "";
+  constructor() {
+    this.key = 0;
+    this.handlers = {};
+  }
+
+  subscribe(handlers: DEventHandler[]): string {
+
+    if (handlers == null || handlers.length == 0) {
+      return "";
+    }
+
+    this.key++;
+    var key = this.key.toString();
+
+    handlers.forEach((v, i) => {
+      v.key = key;
+      this.handlers[v.code].push(v);
+    });
+
+    return key;
   }
 
   unsubscribe(key: string) {
+    if (key == null || key == "") {
+      return;
+    }
 
+    for (let k in this.handlers) {
+      this.handlers[k] = this.handlers[k].filter(ii => ii.key != key);
+    }
   }
 
-  publish(event: Event) {
+  publish(devent: DEvent) {
+    if (devent.code == null || devent.code == "") {
+      return;
+    }
 
+    this.handlers[devent.code].forEach((v, i) => {
+      v.handle(devent);
+    });
   }
 
 }
