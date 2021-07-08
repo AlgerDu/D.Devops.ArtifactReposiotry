030899b1f7e1ae4e546470d08aba572f90647d4e
Alger <AlgerDu@outlook.com>
2021-07-08 23:02:34 +0800

    Update app.component.ts

--- Src/H5Client/src/app/app.component.ts
+++ Src/H5Client/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { AddArtifactRepoComponent } from './modals/add-artifact-repo/add-artifact-repo.component';
 import { ArtifactRepo, ArtifactRepoService } from './services/artifact-repo.service';
+import { DEvent, DEventService } from './services/event.service';
+import { EventCode } from './models/events';
 
 @Component({
   selector: 'app-root',
@@ -9,25 +11,40 @@ import { ArtifactRepo, ArtifactRepoService } from './services/artifact-repo.serv
   styleUrls: ['./app.component.less']
 })
 export class AppComponent implements OnInit {
+
+  eventKey?: string;
   isCollapsed = false;
 
   artifactRepos: ArtifactRepo[] = [];
 
   constructor(
     private artifactRepoService: ArtifactRepoService
-    ,private modalService: NzModalService
-  ){}
+    , private eventService: DEventService
+    , private modalService: NzModalService
+  ) { }
 
   ngOnInit() {
-    this.artifactRepoService.get().subscribe(repos=>{
-      this.artifactRepos = repos;
-    });
+    this.eventKey = this.eventService.subscribe([
+      { code: EventCode.ArtifactRepoCountChange, handle: this.handleRepoChange }
+    ]);
+
+    this.refreash();
   }
 
   showAddArtifactRepoModal(): void {
     this.modalService.create({
       nzTitle: '创建新的仓库',
       nzContent: AddArtifactRepoComponent
+    });
+  }
+
+  handleRepoChange(devent: DEvent) {
+    this.refreash();
+  }
+
+  refreash(){
+    this.artifactRepoService.get().subscribe(repos => {
+      this.artifactRepos = repos;
     });
   }
 }
