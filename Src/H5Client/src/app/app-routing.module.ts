592dcfdd0cc070254898e79265d30aa5cf59e3ce
Alger <AlgerDu@outlook.com>
2021-07-09 23:44:04 +0800

    update routing
    添加 repo 详细信息页面

--- Src/H5Client/src/app/app-routing.module.ts
+++ Src/H5Client/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RepoDetailsComponent } from './pages/repo-details/repo-details.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'repo/:code', component: RepoDetailsComponent },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
