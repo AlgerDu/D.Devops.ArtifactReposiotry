429c73a90af69dde9ab2589341e0f1da134244d3
Alger <AlgerDu@outlook.com>
2021-07-12 22:42:08 +0800

    创建 TableModel<DataType>
    简单创建一个对象，用于简化后续表格的操作

--- Src/H5Client/src/app/models/base.ts
+++ Src/H5Client/src/app/models/base.ts
@@ -1,3 +1,6 @@
+import { NzTableQueryParams } from "ng-zorro-antd/table";
+import { Observable, observable } from 'rxjs';
+
 export interface Result {
     code: number;
     msg?: string;
@@ -21,4 +24,67 @@ export interface SearchResult<DataType> extends Result {
     datas: DataType[];
     totalCount: number;
     page: PageModel;
+}
+
+export interface TableGetDataFunc<DataType> {
+    (model: TableModel<DataType>): Observable<SearchResult<DataType>>;
+}
+
+export class TableModel<DataType> {
+    page: PageModel;
+    total: number;
+
+    condition: string;
+
+    datas: DataType[];
+
+    isLoading: boolean;
+
+    getDateFunc?: TableGetDataFunc<DataType>;
+
+    constructor() {
+        this.page = {
+            index: 1,
+            size: 20
+        };
+        this.condition = "";
+        this.total = 0;
+        this.datas = [];
+        this.isLoading = false;
+    }
+
+    onQueryParamsChange(params: NzTableQueryParams): void {
+
+        console.log(params);
+        const { pageSize, pageIndex, sort, filter } = params;
+
+        this.page.index = params.pageIndex;
+        this.page.size = params.pageSize;
+
+        this.refreash();
+    }
+
+    refreash(): void {
+
+        if (this.getDateFunc == null) {
+            return;
+        }
+
+        this.isLoading = true;
+
+        this.getDateFunc(this).subscribe((rst) => {
+            this.total = rst.totalCount;
+            this.page = rst.page;
+
+            if (rst.datas != null) {
+                this.datas = rst.datas;
+            } else {
+                this.datas = [];
+            }
+        }, (err) => {
+
+        }, () => {
+            this.isLoading = false;
+        });
+    }
 }
--- Src/H5Client/src/app/pages/repo-details/repo-details.component.ts
+++ Src/H5Client/src/app/pages/repo-details/repo-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ArtifactList, ArtifactRepoService, ArtifactRepo } from '../../services/artifact-repo.service';
-import { SearchResult, PageModel } from '../../models/base';
+import { SearchResult, PageModel, TableModel } from '../../models/base';
 import { NzTableQueryParams } from 'ng-zorro-antd/table';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-repo-details',
@@ -13,65 +14,25 @@ export class RepoDetailsComponent implements OnInit {
 
   repo: ArtifactRepo = { code: "", name: "" };
 
-  artifacts: ArtifactList[] = [];
-  page: PageModel = {
-    index: 1,
-    size: 20
-  };
-  condition: string = "";
-  totalCount: number = 0;
-
-  tableIsLoading:boolean = false;
+  artifactTable: TableModel<ArtifactList>;
 
   constructor(
     private route: ActivatedRoute
     , private repoService: ArtifactRepoService
-  ) { }
+  ) {
+    this.artifactTable = new TableModel<ArtifactList>();
+    this.artifactTable.getDateFunc = this.getArtifacts;
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.repo.code = params['code'];
 
-      this.refreash();
+      this.artifactTable.refreash();
     });
   }
 
-  onQueryParamsChange(params: NzTableQueryParams): void {
-    console.log(params);
-    const { pageSize, pageIndex, sort, filter } = params;
-    
-    this.page.index = params.pageIndex;
-    this.page.size = params.pageSize;
-
-    this.refreashTable();
-  }
-
-  refreash() {
-    this.repo.name = this.repo.code;
-
-    this.page = {
-      index: 1,
-      size: 20
-    };
-    this.condition = "";
-    this.artifacts = [];
-    this.totalCount = 0;
-  }
-
-  refreashTable() {
-    this.tableIsLoading = true;
-
-    this.repoService.getArtifacts(this.repo, { page: this.page, condition: this.condition })
-      .subscribe((rst) => {
-        this.tableIsLoading = false;
-
-        if (rst.code != 0) {
-          return;
-        }
-
-        this.page = rst.page;
-        this.artifacts = rst.datas;
-        this.totalCount = rst.totalCount;
-      })
+  getArtifacts(model: TableModel<ArtifactList>): Observable<SearchResult<ArtifactList>> {
+    return this.repoService.getArtifacts(this.repo, { page: model.page, condition: model.condition });
   }
 }
