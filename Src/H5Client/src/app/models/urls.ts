d692a68702582920545047435ae1f80463973b56
Alger <AlgerDu@outlook.com>
2021-07-17 00:02:50 +0800

    Update urls.ts

--- Src/H5Client/src/app/models/urls.ts
+++ Src/H5Client/src/app/models/urls.ts
@@ -1,3 +1,6 @@
 export class ApiUrl{
     static getRepos = "/api/repositorys";
+    static addRepos = "/api/repositorys";
+    static reposBase = "/api/repositorys";
+    static searchArgifact = "api/repositorys/{repoCode}/artifacts/search";
 }
