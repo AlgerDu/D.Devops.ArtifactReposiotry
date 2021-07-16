06d5d0faa200801a265163b18f5c375030811d18
Alger <AlgerDu@outlook.com>
2021-07-16 23:24:33 +0800

    Update Artifact.cs

--- Src/Domain/Domains/Artifact/Artifact.cs
+++ Src/Domain/Domains/Artifact/Artifact.cs
@@ -24,7 +24,7 @@ namespace D.ArtifactReposiotry
         /// <summary>
         /// 归属的制品仓库
         /// </summary>
-        public string RepoName { get; set; }
+        public string RepoCode { get; set; }
 
         /// <summary>
         /// 归属于同一个制品下的二进制文件（比如不同的环境）
