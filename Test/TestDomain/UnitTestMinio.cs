8b5d68012418b751287d916b7ae24c1a64c6959d
Alger <AlgerDu@outlook.com>
2021-07-22 23:38:02 +0800

    add test csproj

---
+++ Test/TestDomain/UnitTestMinio.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDomain
+{
+    [TestClass]
+    public class UnitTestMinio
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
