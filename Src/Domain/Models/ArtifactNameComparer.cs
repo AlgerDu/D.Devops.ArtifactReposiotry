3b024e2aac200ae562dc194868236a91f3435490
Alger <AlgerDu@outlook.com>
2021-07-29 21:27:15 +0800

    Create ArtifactNameComparer.cs

---
+++ Src/Domain/Models/ArtifactNameComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.ArtifactReposiotry
+{
+    public class ArtifactNameComparer : IEqualityComparer<Artifact>
+    {
+        public bool Equals(Artifact x, Artifact y)
+        {
+            return x.Name == y.Name;
+        }
+
+        public int GetHashCode(Artifact obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
