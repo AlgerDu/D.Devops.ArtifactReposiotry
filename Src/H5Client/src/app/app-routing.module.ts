49e765ac19bc2cbca9703e355d07f478b7b7f2ad
Alger <AlgerDu@outlook.com>
2021-08-02 21:13:21 +0800

    Update app-routing.module.ts

--- Src/H5Client/src/app/app-routing.module.ts
+++ Src/H5Client/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RepoDetailsComponent } from './pages/repo-details/repo-details.component';
 import { ArtifactDetailsComponent } from './pages/artifact-details/artifact-details.component';
+import { SearchComponent } from './pages/search/search.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: '', pathMatch: 'full', redirectTo: '/search' },
+  { path: 'search', component: SearchComponent },
   { path: 'repo/:code', component: RepoDetailsComponent },
   { path: 'repo/:code/artifacts/:artifactName', component: ArtifactDetailsComponent }
 ];
