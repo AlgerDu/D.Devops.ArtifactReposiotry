dd787395886c4bc98787a6db3a8edb4c4276c40e
Alger <AlgerDu@outlook.com>
2021-08-04 23:18:30 +0800

    Create TagController.cs

---
+++ Src/Server/Controllers/TagController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D.ArtifactReposiotry.Controllers
+{
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        readonly ILogger _logger;
+
+        public TagController(
+            ILogger<TagController> logger
+            )
+        {
+            _logger = logger;
+        }
+    }
+}
