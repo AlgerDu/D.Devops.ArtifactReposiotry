7958f6d76b70a88a305403224aa4b636228cab3f
Alger <AlgerDu@outlook.com>
2021-08-07 22:23:54 +0800

    simple layout

--- Src/H5Client/src/app/pages/artifact-version/artifact-version.component.ts
+++ Src/H5Client/src/app/pages/artifact-version/artifact-version.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { ArtifactRepoService } from 'src/app/services/artifact-repo.service';
+import { ArtifactSearchModel, ArtifactService } from 'src/app/services/artifact.service';
 
 @Component({
   selector: 'app-artifact-version',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtifactVersionComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  artifact: ArtifactSearchModel = {
+    attributes: {},
+    downloadQuantity: 0,
+    name: "",
+    repoCode: "",
+    tags: [],
+    version: ""
   }
 
+  constructor(
+    private route: ActivatedRoute
+    , private modal: NzModalService
+    , private notification: NzNotificationService
+    , private repoService: ArtifactRepoService
+    , private artifactService: ArtifactService) {
+
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.artifact.repoCode = params['code'];
+      this.artifact.name = params['artifactName'];
+      this.artifact.version = params['version'];
+
+      this.artifact.tags = ["beat","lite"];
+    });
+  }
+
+  deleteClick(){
+
+  }
 }
