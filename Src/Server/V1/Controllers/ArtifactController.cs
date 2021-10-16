1880777d3163bcc12606d0dcbf35b981ca103640
Alger <AlgerDu@outlook.com>
2021-10-16 12:47:48 +0800

    ArtifactController move

--- Src/Server/V1/Controllers/ArtifactController.cs
+++ Src/Server/V1/Controllers/ArtifactController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace D.ArtifactReposiotry.Controllers
+namespace D.ArtifactReposiotry.V1
 {
     [ApiController]
     public class ArtifactController : ControllerBase
