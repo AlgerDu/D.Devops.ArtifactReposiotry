caa61bb7c016413ac9571fb447dc00aa65d7005d
Alger <AlgerDu@outlook.com>
2021-10-16 12:13:09 +0800

    v1 获取仓库列表

---
+++ Src/Server/V1/Controllers/RepositoryController.cs
@@ -0,0 +1,136 @@
+﻿using D.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D.ArtifactReposiotry.V1
+{
+    [ApiController]
+    [Route("api/v1")]
+    public class RepositoryController : ControllerBase
+    {
+        readonly ILogger _logger;
+        readonly IMemoryCache _cache;
+        readonly IEntityAtomicProvider _entityAtomic;
+
+        readonly IArtifactRepoRepository _artifactRepoRepository;
+        readonly IArtifactRepository _artifactRepository;
+
+        public RepositoryController(
+            ILogger<RepositoryController> logger
+            , IArtifactRepoRepository artifactRepoRepository
+            , ICacheProvider cacheProvider
+            , IEntityAtomicProvider entityAtomic
+            , IArtifactRepository artifactRepository
+            )
+        {
+            _logger = logger;
+            _entityAtomic = entityAtomic;
+            _cache = cacheProvider.Get();
+
+            _artifactRepoRepository = artifactRepoRepository;
+            _artifactRepository = artifactRepository;
+        }
+
+        /// <summary>
+        /// 获取所有的仓库信息（以使用的过程中不会有太多的仓库为前提）
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("repositorys")]
+        public ArtifactRepoModel[] GetAll()
+        {
+            var datas = _artifactRepoRepository.Query().ToArray();
+
+            return (from data in datas
+                    select new ArtifactRepoModel
+                    {
+                        Code = data.Name,
+                        Type = data.Type,
+                        Descritpion = ""
+                    }).ToArray();
+        }
+
+        /// <summary>
+        /// 新建一个仓库
+        /// </summary>
+        /// <param name="artifactRepo"></param>
+        /// <returns></returns>
+        [HttpPost("repositorys")]
+        public IResult AddItem(ArtifactRepoModel artifactRepo)
+        {
+            artifactRepo.Code = artifactRepo.Code.ToLower();
+
+            var pk = artifactRepo.PK;
+
+            using (var a = _entityAtomic.Get(pk))
+            {
+                if (a.IsOprting)
+                {
+                    return a.CreateOtherOpertingRst();
+                }
+
+                var exist = _artifactRepoRepository.Query(ii => ii.PK == pk || ii.Name == artifactRepo.Name).Count() > 0;
+
+                if (exist)
+                {
+                    return Result.CreateError($"[{pk}] repo is aleardy exist.");
+                }
+
+                var ok = _artifactRepoRepository.Insert(artifactRepo);
+
+                return ok ? Result.CreateSuccess() : Result.CreateError("insert faild");
+            }
+        }
+
+        [HttpGet("api/repositorys/{repoCode}")]
+        public IResult<ArtifactRepo> GetDetail([FromRoute] string repoCode)
+        {
+            var pk = repoCode.ToLower();
+
+            var repo = _artifactRepoRepository.Get(pk);
+
+            if (repo == null)
+            {
+                return Result.CreateError<ArtifactRepo>($"[{pk}] repo is not exist.");
+            }
+
+            return Result.CreateSuccess(repo);
+        }
+
+        [HttpDelete("api/repositorys/{repoCode}")]
+        public IResult Delete([FromRoute] string repoCode)
+        {
+            var pk = repoCode.ToLower();
+
+            using (var a = _entityAtomic.Get(pk))
+            {
+                if (a.IsOprting)
+                {
+                    return a.CreateOtherOpertingRst();
+                }
+
+                var exist = _artifactRepoRepository.Get(pk);
+
+                if (exist == null)
+                {
+                    return Result.CreateError($"[{pk}] repo is not exist.");
+                }
+
+                var hasArtifacts = _artifactRepository.Query(aa => aa.RepoCode == pk).Count() > 0;
+
+                if (hasArtifacts)
+                {
+                    return Result.CreateError($"[{pk}] delete all artifacts belong this repo first");
+                }
+
+                var ok = _artifactRepoRepository.Delete(pk);
+
+                return ok ? Result.CreateSuccess() : Result.CreateError("delete faild");
+            }
+        }
+    }
+}
---
+++ Src/Server/V1/Models/Repository/ArtifactRepoModel.cs
@@ -0,0 +1,11 @@
+namespace D.ArtifactReposiotry.V1
+{
+    public class ArtifactRepoModel
+    {
+        public string Code { get; set; }
+
+        public ArtifactRepoType Type { get; set; }
+
+        public string Descritpion { get; set; }
+    }
+}
