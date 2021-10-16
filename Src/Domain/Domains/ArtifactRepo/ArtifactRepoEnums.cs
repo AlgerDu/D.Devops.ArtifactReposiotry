713f3f305654fe9347bc90cf7861470dd75292fc
Alger <AlgerDu@outlook.com>
2021-10-16 12:46:51 +0800

    Update ArtifactRepoEnums.cs

--- Src/Domain/Domains/ArtifactRepo/ArtifactRepoEnums.cs
+++ Src/Domain/Domains/ArtifactRepo/ArtifactRepoEnums.cs
@@ -4,10 +4,24 @@ using System.Text;
 
 namespace D.ArtifactReposiotry
 {
+    /// <summary>
+    /// 支持的仓库类型
+    /// </summary>
     public enum ArtifactRepoType
     {
-        Nuget,
+        /// <summary>
+        /// 未知，或者无效
+        /// </summary>
+        None,
 
-        Raw
+        /// <summary>
+        /// 文件
+        /// </summary>
+        Raw,
+
+        /// <summary>
+        /// Nuget
+        /// </summary>
+        Nuget
     }
 }
