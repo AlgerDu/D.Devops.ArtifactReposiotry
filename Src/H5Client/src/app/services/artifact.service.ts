3ec0c380eff8d55eeb05041204674e6cbdd0f7c9
Alger <AlgerDu@outlook.com>
2021-11-14 12:35:27 +0800

    Update artifact.service.ts

--- Src/H5Client/src/app/services/artifact.service.ts
+++ Src/H5Client/src/app/services/artifact.service.ts
@@ -49,6 +49,18 @@ export interface ArtifactModel extends ArtifactBaseModel {
   obejcts: ArtifactObjectModel[];
 }
 
+export interface ArtifactOptDTO {
+  repoCode: string;
+  name: string;
+  version: string;
+}
+
+export interface ArtifactUpdateDTO {
+  depends: DependModel[];
+  tags: string[];
+  attributes: { [key: string]: string; };
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -64,21 +76,53 @@ export class ArtifactService {
     private http: HttpClient
   ) { }
 
+  /**
+   * 查询仓库下的制品
+   * @param repoCode 仓库编号
+   * @param query 查询条件
+   * @returns 符合条件的制品列表
+   */
   search(repoCode: string, query: Search)
     : Observable<SearchResult<ArtifactSearchModel>> {
     var url = `${this.baseUrl}/${repoCode}/search`;
     return this.http.post<SearchResult<ArtifactSearchModel>>(url, query, this.httpOptions);
   }
 
+  /**
+   * 获取制品的版本列表
+   * @param repoCode 仓库编号
+   * @param artifactName 制品名称
+   * @param query 查询条件
+   * @returns 制品的版本列表
+   */
   getVersions(repoCode: string, artifactName: string, query: Search)
     : Observable<SearchResult<ArtifactListModel>> {
     var url = `${this.baseUrl}/${repoCode}/artifacts/${artifactName}/versions`;
     return this.http.post<SearchResult<ArtifactListModel>>(url, query, this.httpOptions);
   }
 
+  /**
+   * 获取制品某一版本的详细信息
+   * @param repoCode 仓库编号
+   * @param artifactName 制品名称
+   * @param artifactVersion 制品版本
+   * @returns 相信信息
+   */
   getDetail(repoCode: string, artifactName: string, artifactVersion: string)
     : Observable<DataResult<ArtifactModel>> {
     var url = `${this.baseUrl}/${repoCode}/artifacts/${artifactName}/v/${artifactVersion}`;
     return this.http.get<DataResult<ArtifactModel>>(url, this.httpOptions);
+  }
+
+  /**
+   * 更新制品的一些可编辑信息
+   * @param artifact 
+   * @param data 
+   * @returns 
+   */
+  update(artifact: ArtifactOptDTO, data: ArtifactUpdateDTO)
+    : Observable<Result> {
+    var url = `${this.baseUrl}/${artifact.repoCode}/artifacts/${artifact.name}/v/${artifact.version}`;
+    return this.http.put<Result>(url, data, this.httpOptions);
   }
 }
