dfa7bf68afad8ee8ae09b615d29abc9a96b73273
Alger <AlgerDu@outlook.com>
2021-11-21 10:09:06 +0800

    tags component

--- Src/H5Client/src/app/common/tags/tags.component.ts
+++ Src/H5Client/src/app/common/tags/tags.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, Output } from '@angular/core';
+import { TagService } from 'src/app/services/tag.service';
+import { isSuccess } from '../../models/base';
 
 @Component({
   selector: 'app-tags',
@@ -7,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TagsComponent implements OnInit {
 
-  constructor() { }
+  @Input("data") tags: string[] = [];
+  @Input("editable") editable: boolean = false;
+
+  inputVisible = false;
+  inputValue = '';
+
+  tagColors: Map<string, string> = new Map<string, string>();
+
+  @ViewChild('inputElement', { static: false }) inputElement?: ElementRef;
+
+  constructor(
+    private tagService: TagService
+  ) { }
 
   ngOnInit(): void {
+    this.tags.forEach(tag => {
+      this.getTagColorAndCache(tag);
+    })
+  }
+
+  handleClose(removedTag: string): void {
+    this.tags = this.tags.filter(tag => tag !== removedTag);
+    this.tagColors.delete(removedTag);
+  }
+
+  showInput(): void {
+    this.inputVisible = true;
+    setTimeout(() => {
+      this.inputElement?.nativeElement.focus();
+    }, 10);
+  }
+
+  handleInputConfirm(): void {
+    if (this.inputValue && this.tags.indexOf(this.inputValue) === -1) {
+      this.tags = [...this.tags, this.inputValue];
+      this.getTagColorAndCache(this.inputValue);
+    }
+    this.inputValue = '';
+    this.inputVisible = false;
+  }
+
+  getTagColorAndCache(tag: string) {
+
+    if (this.tagColors.has(tag)) {
+      return;
+    }
+
+    this.tagService.getTag(tag).subscribe((data) => {
+      this.tagColors.set(tag, data.color)
+    });
   }
 
 }
