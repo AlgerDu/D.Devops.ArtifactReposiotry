8f0b0b8f5fc9cfd26915714af4b2b68f5c46dbc4
algerdu <algerdu@outlook.com>
2023-09-13 23:54:26 +0800

    更新数据库初始化脚本

--- Items/db/init.sql
+++ Items/db/init.sql
@@ -8,3 +8,35 @@ CREATE TABLE artifact (
     is_delete BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE product (
+    id BIGINT,
+    "name" TEXT NOT NULL,
+    "data" JSONB NOT NULL,
+    create_at TIMESTAMPTZ NOT NULL,
+    update_at TIMESTAMPTZ NOT NULL,
+    is_delete BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE product_version (
+    id BIGINT,
+    product_id BIGINT NOT NULL,
+    "version" TEXT NOT NULL,
+    "data" JSONB NOT NULL,
+    create_at TIMESTAMPTZ NOT NULL,
+    update_at TIMESTAMPTZ NOT NULL,
+    is_delete BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tag (
+    id BIGINT,
+    object_type TEXT NOT NULL,
+    object_id BIGINT NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    create_at TIMESTAMPTZ NOT NULL,
+    is_delete BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
