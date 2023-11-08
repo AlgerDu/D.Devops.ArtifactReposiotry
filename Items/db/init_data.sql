958cabec31f4e308590e3df30f82fa9a4f394a81
AlgerDu <AlgerDu@outlook.com>
2023-11-08 10:32:22 +0800

    更新初始化数据库脚本

--- Items/db/init_data.sql
+++ Items/db/init_data.sql
@@ -4,6 +4,8 @@ VALUES
 (1,0,'product','object','产品','{}',current_timestamp,current_timestamp,true,false),
 (2,0,'version','object','版本','{}',current_timestamp,current_timestamp,true,false),
 (3,0,'artifact','object','制品','{}',current_timestamp,current_timestamp,true,false),
-(4,1,'name','string','名称，唯一','{}',current_timestamp,current_timestamp,true,false),
-(5,1,'description','string','描述','{"isExt":true}',current_timestamp,current_timestamp,true,false)
-;
+(4,1,'name','string','名称，唯一','{"isExt":false,"isKey":true,"editable":true}',current_timestamp,current_timestamp,true,false),
+(5,1,'description','string','描述','{"isExt":true,"isKey":false,"editable":true}',current_timestamp,current_timestamp,true,false),
+(6, 2, 'version', 'string', '版本号', '{"isExt":false,"isKey":true,"editable":true}', current_timestamp, current_timestamp, true, false),
+(7,2,'description','string','描述','{"isExt":true,"isKey":false,"editable":true}',current_timestamp,current_timestamp,true,false);
+(8,2,'productName','string','关联的产品','{"isExt":true,"isKey":false,"editable":true,"extCreate":true}',current_timestamp,current_timestamp,true,false);
